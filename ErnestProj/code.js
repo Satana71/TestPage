7ec380b9f51c34050839617e2c33898fecfcce09
Satana71 <56357249+Satana71@users.noreply.github.com>
2020-02-27 15:13:09 +0300

    Add files via upload

---
+++ ErnestProj/code.js
@@ -0,0 +1,99 @@
+let slider = document.querySelector('.slider'); //получение общего слайдера
+let sliderLenta = slider.querySelector('.slider-lenta'); // общая лента
+let sliderItems = slider.querySelectorAll('.slider-lenta-item.real'); // получаем настоящие элементы
+let sliderBullets = document.querySelectorAll('.slider-bullets div'); // здесь у нас буллетсы
+
+let nowShowIndex = 0; // наш изначальный индекс
+bullets_draw(nowShowIndex); // изначальная отрисовка
+
+arr_left.onclick = function () {
+    if (sliderLenta.classList.contains('animated')) {
+        return;
+    }
+
+    sliderLenta.classList.add('animated');
+    if (nowShowIndex <= 0) {
+        nowShowIndex = nowShowIndex - 1;
+
+        $(sliderLenta).animate({
+            left: -100 * (nowShowIndex + 1) + "%"
+        }, 600, function () {
+
+            nowShowIndex = sliderItems.length - 1;
+            sliderLenta.style.left = -100 * (nowShowIndex + 1) + "%";
+            sliderLenta.classList.remove('animated');
+            bullets_draw(nowShowIndex);
+        });
+    } else {
+        nowShowIndex = nowShowIndex - 1;
+        $(sliderLenta).animate({
+            left: -100 * (nowShowIndex + 1) + "%"
+        }, 600, function () {
+            sliderLenta.classList.remove('animated');
+        })
+    }
+    bullets_draw(nowShowIndex);
+};
+
+
+arr_right.onclick = function () {
+
+    sliderLenta.classList.add('animated');
+    if (nowShowIndex >= sliderItems.length - 1) {
+        nowShowIndex = nowShowIndex + 1;
+        $(sliderLenta).animate({
+            left: -100 * (nowShowIndex + 1) + "%"
+
+        }, 600, function () {
+            nowShowIndex = 0;
+
+            sliderLenta.style.left = 'left', -100 * (nowShowIndex + 1) + "%";
+
+            sliderLenta.classList.remove('animated');
+            bullets_draw(nowShowIndex);
+        });
+        if (sliderLenta.classList.contains('animated')) {
+            return;
+        }
+    } else {
+        nowShowIndex = nowShowIndex + 1;
+        $(sliderLenta).animate({
+            left: -100 * (nowShowIndex + 1) + "%"
+        }, 600, function () {
+            sliderLenta.classList.remove('animated');
+            bullets_draw(nowShowIndex)
+        })
+        if (sliderLenta.classList.contains('animated')) {
+            return;
+        }
+    }
+};
+
+
+sliderBullets.forEach(function (bullet) {
+    bullet.onclick = function () {
+        if (sliderLenta.classList.contains('animated')) {
+            return;
+        }
+        sliderLenta.classList.add('animated');
+        let bullet_index = bullet.getAttribute('id');
+        bullets_draw(bullet_index);
+        nowShowIndex = +bullet_index;
+        $(sliderLenta).animate({
+            left: -100 * (nowShowIndex + 1) + "%"
+        }, 600, function () {
+            sliderLenta.classList.remove('animated');
+        })
+    };
+
+});
+
+
+
+
+function bullets_draw(show_index) {
+    for (let i = 0; i < sliderBullets.length; i++) {
+        sliderBullets[i].style.backgroundColor = "white";
+    }
+    sliderBullets[show_index].style.backgroundColor = "orange";
+}
